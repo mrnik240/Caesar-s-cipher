ae2e92e1ccfe7b1229ddabee308b31a7c39416d2
Flame Sign <Flame Sign@DESKTOP-UPJMJ81>
2022-11-12 05:35:12 +0700

    Добавьте файлы проекта.

---
+++ Caesar's cipher/Caesar_s_cipher.py
@@ -0,0 +1,23 @@
+alphabet_rus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+alphabet_rus_upper = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+alphabet_eng = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+alphabet_eng_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+text = list(input("Введите предложение для шифрования...\t"))
+bias = int(input("Введите шаг...\t"))
+
+for i in range(0, len(text)):
+    if text[i].islower():
+        if text[i].isascii():
+            n = alphabet_eng.find(text[i])
+            text[i] = alphabet_eng[n + bias]
+        else:
+            n = alphabet_rus.find(text[i])
+            text[i] = alphabet_rus[n + bias]
+    elif text[i].isupper():
+        if text[i].isascii():
+            n = alphabet_eng_upper.find(text[i])
+            text[i] = alphabet_eng_upper[n + bias]
+        else:
+            n = alphabet_rus_upper.find(text[i])
+            text[i] = alphabet_rus_upper[n + bias]
+print("".join(text))
